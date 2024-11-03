a43070280cf6de4c095b49cca248e8cc55a2114c
Raphael <raphabrunetto@gmail.com>
2024-11-03 19:07:09 -0300

    atualizações

--- src/main/kotlin/character/character.kt
+++ src/main/kotlin/character/character.kt
@@ -1,14 +1,11 @@
 package org.example.character
 
-
 import org.example.Races.RaceStrategy
 import org.example.classes.CharacterClass
 import org.example.utils.Calculator
 
-
 class Character(
-
-    val id : Int =0,
+    val id: Int = 0,
     val name: String,
     val race: RaceStrategy,
     val characterClass: CharacterClass,
@@ -26,4 +23,18 @@ class Character(
     val adjustedAttributes: Attributes
         get() = _adjustedAttributes
 
+    // Companion object com metodo factory
+    companion object {
+        fun create(
+            id: Int,
+            name: String,
+            race: RaceStrategy,
+            characterClass: CharacterClass,
+            alignment: String,
+            background: String,
+            baseAttributes: Attributes
+        ): Character {
+            return Character(id, name, race, characterClass, alignment, background, baseAttributes)
+        }
+    }
 }
